af540f3caecfe99370f128d23e692213693de954
tapsa <tapsa872@gmail.com>
2018-01-16 21:00:53 +0200

    Added functionality to add, update and delete tasks, also saved into mlab db

--- MEANtodo/src/app/components/tasks/tasks.component.ts
+++ MEANtodo/src/app/components/tasks/tasks.component.ts
@@ -23,7 +23,42 @@ export class TasksComponent implements OnInit {
 
   addTask(event) {
     event.preventDefault();
-    console.log(this.title);
+    var newTask = {
+      title: this.title,
+      isDone: false
+    }
+
+    this._taskService.addTask(newTask) 
+        .subscribe(task => {
+          this.tasks.push(task);
+          this.title = "";
+        });
+  }
+
+  deleteTask(id) {
+    var tasks = this.tasks;
+
+    this._taskService.deleteTask(id).subscribe(data => {
+      if(data.n == 1) {
+        for(var i = 0; i < tasks.length; i++) {
+          if(tasks[i]._id == id) {
+            tasks.splice(i, 1);
+          }
+        }
+      }
+    });
+  }
+
+  updateStatus(task) {
+    var _task = {
+      _id:task._id,
+      title: task.title,
+      isDone: !task.isDone
+    };
+
+    this._taskService.updateStatus(_task).subscribe(data => {
+      task.isDone = !task.isDone;
+    });
   }
 
 
--- MEANtodo/src/app/services/task.service.ts
+++ MEANtodo/src/app/services/task.service.ts
@@ -13,4 +13,23 @@ export class TaskService {
      return this._http.get("/api/tasks").map(res => res.json());
    }
 
+   addTask(newTask) {
+     var headers = new Headers();
+     headers.append("Content-Type", "application/json");
+     return this._http.post("/api/task", JSON.stringify(newTask), { headers: headers })
+            .map(res => res.json());
+   }
+
+   deleteTask(id) {
+     return this._http.delete("/api/task/"+id)
+            .map(res => res.json());
+   }
+
+   updateStatus(task) {
+    var headers = new Headers(); 
+    headers.append("Content-Type", "application/json");
+    return this._http.put("/api/task/"+task._id, JSON.stringify(task), { headers: headers })
+           .map(res => res.json());
+   }
+
 }
